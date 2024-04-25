91d14dfff27f7c93a2b8c5f49260d140988d8ad2
Agfiras <74111285+Agfiras@users.noreply.github.com>
2024-04-25 14:22:26 +0200

    Update Install_Driver.ps1

--- Install_Driver.ps1
+++ Install_Driver.ps1
@@ -1,5 +1,5 @@
 # Define the path to the INSTALL.BAT file
-$installScriptPath = "C:\Users\Adok\source\repos\Agfiras\Battery-Driver_Hub\x64\Debug\simbatt\INSTALL.BAT"
+$installScriptPath = "$env:GITHUB_WORKSPACE\Battery-Driver_Hub\x64\Debug\simbatt\INSTALL.bat"
 
 # Check if the file exists
 if (Test-Path $installScriptPath -PathType Leaf) {
