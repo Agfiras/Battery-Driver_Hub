29ea959aeec8b76ddcc2b0890624313d0f1cdffa
Firas Ajengui <74111285+Agfiras@users.noreply.github.com>
2024-07-15 21:04:24 +0200

    Update Main.cpp

--- BatteryConfig/Main.cpp
+++ BatteryConfig/Main.cpp
@@ -6,7 +6,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
     CBatteryService service(TEXT("BatteryService"));
     if (!CServiceBase::Run(service)) {
         DWORD dwError = GetLastError();
-        wchar_t errMsg[256];   
+        wchar_t errMsg[256];     
         swprintf_s(errMsg, L"Service failed to run with error: %lu\n", dwError);
         MessageBox(NULL, errMsg, L"Error", MB_OK | MB_ICONERROR);
     }
